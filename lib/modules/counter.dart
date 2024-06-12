005502cf031a1fa4e4d506e1929bb8f7b0ae7de5
Agustin <agustinspaccesi@gmail.com>
2024-06-12 17:39:23 +0200

    AppBar with title added and SafeArea removed because not needed anymore

--- lib/modules/counter.dart
+++ lib/modules/counter.dart
@@ -19,60 +19,61 @@ class _CounterPageState extends State<CounterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
-          child: Column(
-            children: [
-              Form(
-                key: _formKey,
-                child: TextFormField(
-                  controller: _initialCounterController,
-                  keyboardType: TextInputType.number,
-                  inputFormatters: <TextInputFormatter>[
-                    FilteringTextInputFormatter.digitsOnly
-                  ],
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Empty value it is not permitted to set initial counter";
-                    }
-                    try {
-                      int.parse(value);
-                      return null;
-                    } catch (e) {
-                      return "Something went wrong! Try again.";
-                    }
-                  },
-                  decoration: const InputDecoration(
-                    labelText: 'Initial counter',
-                  ),
+      appBar: AppBar(
+        title: const Text("Counter"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(defaultPadding),
+        child: Column(
+          children: [
+            Form(
+              key: _formKey,
+              child: TextFormField(
+                controller: _initialCounterController,
+                keyboardType: TextInputType.number,
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.digitsOnly
+                ],
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Empty value it is not permitted to set initial counter";
+                  }
+                  try {
+                    int.parse(value);
+                    return null;
+                  } catch (e) {
+                    return "Something went wrong! Try again.";
+                  }
+                },
+                decoration: const InputDecoration(
+                  labelText: 'Initial counter',
                 ),
               ),
-              const SizedBox(height: defaultGap),
-              ElevatedButton(
-                onPressed: () {
-                  if (!_formKey.currentState!.validate()) return;
-                  setState(() {
-                    counter = int.parse(_initialCounterController.text);
-                    _initialCounterController.text = "";
-                  });
-                },
-                child: const Text('Set counter'),
-              ),
-              const SizedBox(height: defaultGap),
-              Text(
-                'Click counter: ${counter.toString()}',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-              const SizedBox(height: defaultGap),
-              ElevatedButton(
-                onPressed: () => setState(() {
-                  counter += 1;
-                }),
-                child: const Text('Add 1 to counter'),
-              ),
-            ],
-          ),
+            ),
+            const SizedBox(height: defaultGap),
+            ElevatedButton(
+              onPressed: () {
+                if (!_formKey.currentState!.validate()) return;
+                setState(() {
+                  counter = int.parse(_initialCounterController.text);
+                  _initialCounterController.text = "";
+                });
+              },
+              child: const Text('Set counter'),
+            ),
+            const SizedBox(height: defaultGap),
+            Text(
+              'Click counter: ${counter.toString()}',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const SizedBox(height: defaultGap),
+            ElevatedButton(
+              onPressed: () => setState(() {
+                counter += 1;
+              }),
+              child: const Text('Add 1 to counter'),
+            ),
+          ],
         ),
       ),
     );
