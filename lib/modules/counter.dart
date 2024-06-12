152628d746274e8c1301a40c6cd4642291968b26
Agustin <agustinspaccesi@gmail.com>
2024-06-12 17:25:48 +0200

    Counter logic implememented

--- lib/modules/counter.dart
+++ lib/modules/counter.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pt1/app/theme/theme_constants.dart';
 
 class CounterPage extends StatefulWidget {
@@ -9,34 +10,64 @@ class CounterPage extends StatefulWidget {
 }
 
 class _CounterPageState extends State<CounterPage> {
+  int counter = 0;
+  final TextEditingController _initialCounterController =
+      TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    const String counter = '3';
     return Scaffold(
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
           child: Column(
             children: [
-              const TextField(
-                decoration: InputDecoration(
-                  labelText: 'Initial counter',
+              Form(
+                key: _formKey,
+                child: TextFormField(
+                  controller: _initialCounterController,
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly
+                  ],
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Empty value is is not permitted to set initial counter";
+                    }
+                    try {
+                      int.parse(value);
+                      return null;
+                    } catch (e) {
+                      return "Something went wrong! Try again.";
+                    }
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'Initial counter',
+                  ),
                 ),
               ),
               const SizedBox(height: defaultGap),
               ElevatedButton(
                 onPressed: () {
-                  // TODO Add your onPressed code here!
+                  if (!_formKey.currentState!.validate()) return;
+                  setState(() {
+                    counter = int.parse(_initialCounterController.text);
+                  });
                 },
                 child: const Text('Set counter'),
               ),
               const SizedBox(height: defaultGap),
-              const Text('click counter $counter'),
+              Text(
+                'Click counter: ${counter.toString()}',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
               const SizedBox(height: defaultGap),
               ElevatedButton(
-                onPressed: () {
-                  // TODO Add your onPressed code here!
-                },
+                onPressed: () => setState(() {
+                  counter += 1;
+                }),
                 child: const Text('Add 1 to counter'),
               ),
             ],
